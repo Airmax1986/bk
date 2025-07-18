c0707801eb2510c27bc007b6bf0cafd341c5492c
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-18 21:51:33 +0200

    feat: Add insights section ID for navigation

--- components/ThoughtLeadershipTeaser.js
+++ components/ThoughtLeadershipTeaser.js
@@ -36,7 +36,7 @@ export default function ThoughtLeadershipTeaser() {
   }, [insights.length])
 
   return (
-    <section className="py-20 bg-white">
+    <section id="insights" className="py-20 bg-white">
       <div className="container mx-auto px-4">
         <div className="max-w-6xl mx-auto">
           
